50d81741640c477c3902f415135a0c01374c7466
Miguel319 <miguelp90.319@gmail.com>
2019-12-30 18:52:45 -0400

    API setup with Node & Express

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home() {
+import React from "react";
+import axios from "axios";
+
+function Home({ products }) {
+  console.log(products);
+
+ 
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  // Fetch data on server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+
+  return { products: response.data };
+  // Note this object will be merged with existing props
+};
 
 export default Home;
