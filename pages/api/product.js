61d2c632e50d3be3834090c2475e3b5917411c6a
Miguel319 <miguelp90.319@gmail.com>
2019-12-31 16:38:35 -0400

    Product deletion

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,8 +1,28 @@
-import Product from '../../models/Product';
+import Product from "../../models/Product";
 
 export default async (req, res) => {
-    const { _id } = req.query;
-    const product = await Product.findOne({  _id });
-
-    res.status(200).json(product);
+  switch (req.method) {
+    case "GET":
+      await handleGetReq(req, res);
+      break;
+    case "DELETE":
+      await handleDeleteReq(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+  }
 };
+
+const handleGetReq = async (req, res) => {
+  const { _id } = req.query;
+  const product = await Product.findOne({ _id });
+
+  res.status(200).json(product);
+};
+
+const handleDeleteReq = async (req, res) => {
+    const { _id } = req.query;
+    await Product.findOneAndDelete({_id});
+    res.status(204)
+        .json({});
+}
