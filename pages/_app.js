f4393872a01a0cffb8ed74a018e1937b667051b9
Miguel319 <miguelp90.319@gmail.com>
2020-01-03 12:56:28 -0400

    Authorization: Route protection based on user permissions

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,41 @@
 import App from "next/app";
 import Layout from "../components/_App/Layout";
+import { parseCookies, destroyCookie } from "nookies";
+import { redirectUser } from "../utils/auth";
+import baseUrl from "../utils/baseUrl";
+import axios from "axios";
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
+    const { token } = parseCookies(ctx);
+
     let pageProps = {};
 
     if (Component.getInitialProps)
       pageProps = await Component.getInitialProps(ctx);
+
+    if (!token) {
+      const isProtectedRoute =
+        ctx.pathname === "/account" || ctx.pathname === "/create";
+
+      if (isProtectedRoute) {
+        redirectUser(ctx, "/login");
+      }
+    } else {
+      try {
+        const url = `${baseUrl}/api/account`;
+        const payload = { headers: { Authorization: token } };
+        const response = await axios.get(url, payload);
+
+        const user = response.data;
+        pageProps.user = user;
+      } catch (e) {
+        console.error("Unable to get current user", e);
+        destroyCookie(ctx, 'token');
+        redirectUser(ctx, '/login');
+
+      }
+    }
 
     return { pageProps };
   }
@@ -15,7 +44,7 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Layout>
+      <Layout {...pageProps}>
         <Component {...pageProps} />
       </Layout>
     );
